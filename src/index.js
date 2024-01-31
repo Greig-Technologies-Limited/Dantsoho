befd1f0bf01fd3359f2fb2a37ed6893e26d435f3
christopher020 <uchennachris020@gmail.com>
2024-01-31 10:39:52 +0000

    Navigation Bar update

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import App from './App';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import AboutPage from './routes/AboutPage';
 import FoundationPage from './routes/FoundationPage';
+import ScrollToTop from './components/ScrollToTop';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
-  <Routes>
-    <Route path='/' element={<App/>}/>
-    <Route path='/AboutPage' element={<AboutPage/>}/>
-    <Route path='/FoundationPage' element={<FoundationPage/>}/>
-  </Routes>
+    <ScrollToTop />
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/AboutPage' element={<AboutPage/>}/>
+      <Route path='/FoundationPage' element={<FoundationPage/>}/>
+    </Routes>
   </HashRouter>
 );
 
